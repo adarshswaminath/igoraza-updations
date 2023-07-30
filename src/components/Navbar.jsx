01a9ab77305b254ce7f9e26585d3f1f94b388abf
Adarsh <adarshswaminath7@gmail.com>
2023-07-30 08:43:25 +0530

    sample hover added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,51 @@
-import { useState } from "react";
-
+import React, { useState } from "react";
 import { close, menu, logo } from "../assets";
 import icon from "../assets/icon.png";
-// import logo from "../assets/asset30.png";
-
 import { navLinks } from "../constants";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
+  const [isHovering, setIsHovering] = useState(""); // State for hover effect
+
+  const handleMouseEnter = (title) => {
+    setIsHovering(title);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering("");
+  };
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <div className="flex items-center">
-        <img src={icon} alt="hoobank" className="w-[58px] h-[55px] " />
-        <img src={logo} alt="hoobank" className="w-[148px] h-[65px] " />
+        <img src={icon} alt="hoobank" className="w-[58px] h-[55px]" />
+        <img src={logo} alt="hoobank" className="w-[148px] h-[65px]" />
       </div>
-
-      <ul className=" list-none sm:flex gap-4 hidden justify-end items-center flex-1 text-white">
-        <li className="font-poppins font-bold cursor-pointer text-[16px]">
-          <a href="#" className="hover:text-dimWhite">Home</a>
-        </li>
-        <li className="font-poppins font-bold cursor-pointer text-[16px] ">
-          <a href="#" className="hover:text-dimWhite">IT Services</a>
-        </li>
-        <li className="font-poppins font-bold cursor-pointer text-[16px] ">
-          <a href="#" className="hover:text-dimWhite">Portfolio</a>
-        </li>
-        <li className="font-poppins font-bold cursor-pointer text-[16px] ">
-          <a href="#" className="hover:text-dimWhite" >Careers</a>
-        </li>
-        <li className="font-poppins font-bold cursor-pointer text-[16px] ">
-          <a href="#" className="hover:text-dimWhite">Contact Us</a>
-        </li>
+      {/* desktop view */}
+      <ul className="list-none sm:flex gap-4 hidden justify-end items-center flex-1 text-white">
+        {navLinks.map((nav) => (
+          <li
+            key={nav.id}
+            className="font-poppins font-bold cursor-pointer text-[16px]"
+          >
+            <a
+              href={`#${nav.id}`}
+              className="hover:text-dimWhite relative"
+              onMouseEnter={() => handleMouseEnter(nav.title)}
+              onMouseLeave={handleMouseLeave}
+            >
+              {nav.title}
+              {isHovering === nav.title && (
+                 <div className="absolute left-1/2 transform -translate-x-1/2 bg-black-gradient px-32 py-32 rounded-lg border border-white shadow-md mt-2">
+                 <h3 className="text-3xl text-white">Sample </h3>
+                 {/* Add more content as needed */}
+               </div>
+              )}
+            </a>
+          </li>
+        ))}
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
